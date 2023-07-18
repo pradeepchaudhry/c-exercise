23a8207a59c823b70da40eccfd4305d66c7e5c95
Pradeep kumar <121189211+pradeepchaudhry@users.noreply.github.com>
2023-07-18 11:05:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more informationclass program
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse = 0;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse of Entered Number is : "+reverse);
+        
+ 
+        }
+    }
+}
